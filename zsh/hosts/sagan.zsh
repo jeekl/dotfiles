15916827888584e9a0a69363c460dbd64df61f18
Jeff Eklund <jeff.eklund@klarna.com>
2012-08-22 22:39:10 +0200

    zsh: commit host specific settings for sagan

---
+++ zsh/hosts/sagan.zsh
@@ -0,0 +1,5 @@
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern root)
+ZSH_HIGHLIGHT_STYLES[root]='bg=red'
+ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
+
